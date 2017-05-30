160809eaf3801e705f638f8034d283ba7add3755
kit.perez <kit.perez@gmail.com>
2017-05-30 15:32:59 +1000

    initial commit: lab else-if solution

---
+++ Enrique/wk01/2-tue/LAB-js-day2-else-if/else-if-solution.js
@@ -0,0 +1,11 @@
+console.log('ready loopy');
+
+var d = new Date();
+
+if (2015 === d.getFullYear()) {
+  console.log("I'm in the present!");
+} else if (2015 <= d.getFullYear()){
+  console.log("Whoa! Blast from the past!");  
+} else { 
+  console.log("Greatings from the future!");
+}
