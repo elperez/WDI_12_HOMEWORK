e152e53e1b47dbc4d4009610b7607a42d3bc05be
kit.perez <kit.perez@gmail.com>
2017-05-31 09:39:27 +1000

    updated else-if solution

--- Enrique/wk01/2-tue/LAB-js-day2-else-if/else-if-solution.js
+++ Enrique/wk01/2-tue/LAB-js-day2-else-if/else-if-solution.js
@@ -2,9 +2,9 @@ console.log('ready loopy');
 
 var date = new Date();
 
-if (2015 === d.getFullYear()) {
+if (2015 === date.getFullYear()) {
   console.log("I'm in the present!");
-} else if (2015 <= d.getFullYear()){
+} else if (2015 <= date.getFullYear()){
   console.log("Whoa! Blast from the past!");  
 } else { 
   console.log("Greatings from the future!");
